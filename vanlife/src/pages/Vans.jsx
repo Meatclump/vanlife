6f2ac89ce249523703e00ec7ab48dd96c2380b83
TechPersson <Hepe22@hotmail.com>
2023-05-29 13:07:19 -0500

    Implement data fetch
    Make the fetch request and temporarily print list to Vans view

--- vanlife/src/pages/Vans.jsx
+++ vanlife/src/pages/Vans.jsx
@@ -1,7 +1,26 @@
+import React from 'react'
+
 export default function Vans() {
+    const [vanArray, setVanArray] = React.useState([])
+    const [vanElements, setVanElements] = React.useState(vanArray)
+
+    React.useEffect(() => {
+        fetch('/api/vans')
+        .then(response => response.json())
+        .then(data => setVanArray(data.vans))
+    }, [])
+
+    React.useEffect(() => {
+        setVanElements(vanArray.map(van => <li key={van.id}>{van.name}</li>))
+    }, [vanArray])
+
+
     return (
         <div className="van-container">
             <h1>Explore our van options</h1>
+            <ul>
+            {vanElements}
+            </ul>
         </div>
     )
 }
